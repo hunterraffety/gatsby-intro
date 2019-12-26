1fd0d658ff5dbf46e9c91f0505938469e336eb9c
Hunter Raffety <hsraffety@gmail.com>
2019-12-26 15:16:53 -0700

    updates to gatsby project. includes node api for gatsby

---
+++ src/hooks/usePosts.js
@@ -0,0 +1,27 @@
+import { graphql, useStaticQuery } from 'gatsby'
+
+const usePosts = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMdx {
+        nodes {
+          frontmatter {
+            author
+            slug
+            title
+          }
+          excerpt
+        }
+      }
+    }
+  `)
+
+  return data.allMdx.nodes.map(post => ({
+    title: post.frontmatter.title,
+    author: post.frontmatter.author,
+    slug: post.frontmatter.slug,
+    excerpt: post.excerpt,
+  }))
+}
+
+export default usePosts
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import Layout from '../components/layout'
+import usePosts from '../hooks/usePosts'
 
-export default () => (
-  <Layout>
-    <h1>Crafts by Carolyn</h1>
-    <p>Site under development! Coming soon.</p>
-    <Link to="/about/">&rarr; About</Link>
-  </Layout>
-)
+import Layout from '../components/layout'
+import PostPreview from '../components/post-preview'
+
+export default () => {
+  const posts = usePosts()
+
+  return (
+    <Layout>
+      <h1>Crafts by Carolyn</h1>
+      <p>Site under development! Coming soon.</p>
+      <Link to="/about/">&rarr; About</Link>
+
+      <h2>Read the blog</h2>
+      {posts.map(post => (
+        <PostPreview key={post.slug} post={post} />
+      ))}
+    </Layout>
+  )
+}
