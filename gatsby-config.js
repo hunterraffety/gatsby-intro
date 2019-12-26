3f1c6d116ec1d717fbb88704f2db25a65b5dbd46
Hunter Raffety <hsraffety@gmail.com>
2019-12-26 11:49:33 -0700

    whoops

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,8 @@
 module.exports = {
+  siteMetadata: {
+    title: 'Crafts by Carolyn',
+    description:
+      "The online boutique for Carolyn Raffety's crafts and knick-knacks",
+  },
   plugins: ['gatsby-plugin-emotion'],
-};
+}
