0a0d7f6d258d996266301b9a114c27ba854c8537
Hunter Raffety <hsraffety@gmail.com>
2019-12-26 12:24:35 -0700

    updating packages, adding wave

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,5 +4,18 @@ module.exports = {
     description:
       "The online boutique for Carolyn Raffety's crafts and knick-knacks",
   },
-  plugins: ['gatsby-plugin-emotion'],
+  plugins: [
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        defaultLayouts: {
+          default: require.resolve('./src/components/layout.js'),
+        },
+        gatsbyRemarkPlugins: ['gatsby-remark-images'],
+        plugins: ['gatsby-remark-images'],
+      },
+    },
+  ],
 }
