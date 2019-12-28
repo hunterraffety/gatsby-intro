200c308c5c9d5b7da7c2bc8b9f14d99cdd517164
Hunter Raffety <hsraffety@gmail.com>
2019-12-27 18:34:52 -0700

    hero image

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,16 +1,54 @@
 import React from 'react'
-import styled from '@emotion/core'
+import styled from '@emotion/styled'
 import { Link, graphql, useStaticQuery } from 'gatsby'
+
+const ImageBackground = styled.div`
+  background-image: url('/images/wrx.jpg');
+  background-position: top 20% center;
+  background-size: cover;
+  height: 50vh;
+
+  + * {
+    margin-top: 0;
+  }
+`
+
+const TextBox = styled.div`
+  background-image: linear-gradient(to top, #ddbbffdd, 2rem, #ddbbff00);
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  justify-content: flex-end;
+  padding: 0 calc((100vw - 550px) / 2) 2rem;
+  width: 100%;
+
+  h1 {
+    text-shadow: 1px 1px 3px #eeddff66;
+    font-size: 2.25rem;
+  }
+
+  p,
+  a {
+    color: #222;
+    margin-top: 0;
+  }
+
+  a {
+    margin-top: 0.5rem;
+  }
+`
 
 const Hero = () => {
   return (
-    <div>
-      <h1>Crafts by Carolyn &hearts;</h1>
-      <p>
-        Hello Arizona (and beyond!){' '}
-        <Link to="/about/">Learn about my things</Link>
-      </p>
-    </div>
+    <ImageBackground>
+      <TextBox>
+        <h1>Crafts by Carolyn &hearts;</h1>
+        <p>
+          Hello Arizona (and beyond!){' '}
+          <Link to="/about/">Learn about my things</Link>
+        </p>
+      </TextBox>
+    </ImageBackground>
   )
 }
 
